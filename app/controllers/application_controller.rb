67d2332c17a55a04b4c369ad43980e0e6aa74de0
taka.aca <taka.aca@techacademy.jp>
2020-05-13 12:07:17 +0000

    last post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :require_user_logged_in, only: [:index, :show]
   
   include SessionsHelper
 
@@ -8,9 +9,5 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_url
     end
-  end
-
-  def counts(user)
-    @count_tasks = user.tasks.count
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,7 @@ class TasksController < ApplicationController
     
     
   def index
-    if logged_in?
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
